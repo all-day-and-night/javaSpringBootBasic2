e410e2946efbd353a6cbb5eb999c3a1068ddcf02
all-day-and-night <jwug0821@gmail.com>
2021-11-20 22:44:55 +0900

    beanDefinition test with xml

--- core/src/test/java/hello/core/beandefinition/beanDefinitionTest.java
+++ core/src/test/java/hello/core/beandefinition/beanDefinitionTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.lang.reflect.GenericArrayType;
 
 public class beanDefinitionTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-    
+    //appConfig
+    //AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
     @Test
     @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean(){
