c69a571bbecbf4e2c3c6e99ea95bc7a745ef57fa
all-day-and-night <jwug0821@gmail.com>
2021-11-25 23:15:40 +0900

    @Configuration keep singleton pattern

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Bean만 사용해도 스프링 빈으로 등록되지만 싱글톤은 보장하지 않는다
 
 Configuration을 사용하게 된다면 스프링에서 자동으로 싱글톤을 보장하여 실행한다.
+
  */
 @Configuration
 public class AppConfig {
@@ -25,19 +26,19 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService(){
-        System.out.println("call AppConfig.memberService");
+        //System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
-        System.out.println("call AppConfig.memberRepository");
+        //System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
-        System.out.println("call AppConfig.orderService");
+        //System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
@@ -45,7 +46,7 @@ public class AppConfig {
     public DiscountPolicy discountPolicy(){
         //->할인 정책을 쉽게 교체, 인터페이스를 implements하는 class 구현 후 교체
         //return new FixDiscountPolicy();
-        System.out.println("call AppConfig.discountPolicy");
+        //System.out.println("call AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 
