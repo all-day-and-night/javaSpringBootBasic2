95351663d7184ae5b33b896806f60f108b13d3e5
all-day-and-night <jwug0821@gmail.com>
2021-11-26 00:16:58 +0900

    Using @Configuration and @ComponentScan

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.FilterType;
 //ComponentScan @ Annotation이 붙은 모든 컴포넌트 스캔 and except prev Configuration in AppConfig
 @Configuration
 @ComponentScan(
+        //
+        //basePackages = "hello.core.member", 검색할 위치 지정 -> 추천 : 프로젝트의 최상단 위치 지정
+        //basePackages = {"hello.core", "hello.service"} 여러 시작 위치 지정 가능
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
