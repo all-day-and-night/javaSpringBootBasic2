56e73207f8da66cacbceec690e2d623a4e356049
all-day-and-night <jwug0821@gmail.com>
2021-11-25 23:41:31 +0900

    update for using @ComponentScan

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //인터페이스만이 아니라 클래스도 참조하는 문제가 생김 ->  OCP위반(역할과 기능구현 분리)
     private final MemberRepository memberRepository;
 
+    //@Component가 붙은 생성자는 @Autowired로 연결해서 자동으로 의존관계 주입
+    @Autowired
     public MemberServiceImpl(MemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
