84310d1e3ba3139e74bdc116e87a4a927d863c28
all-day-and-night <jwug0821@gmail.com>
2021-12-09 21:46:34 +0900

    Controller for request log using ProxyMod

--- core/src/main/java/hello/core/web/LogDemoController2.java
+++ core/src/main/java/hello/core/web/LogDemoController2.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController2 {
